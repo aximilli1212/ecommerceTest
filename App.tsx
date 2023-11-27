4b9c30ec0b5db8f85f8d3105d23204a7f701e4d3
Emmanuel Sackey <aximilli1212@gmail.com>
2023-11-27 02:24:18 +0000

    Add monserrat font and react navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require('./assets/fonts/Montserrat-Regular.ttf'),
+  });
+
   return (
     <View style={styles.container}>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
